31442b2e6c52e424da96d2acea1f348798f6c6c9
belalex26 <alex.belova26@gmail.com>
2020-08-27 16:53:35 +0300

    сетка правки + меню + слайдер

---
+++ source/js/main.js
@@ -0,0 +1,14 @@
+let navMain = document.querySelector(".menu");
+let navToggle = document.querySelector(".menu__burger");
+
+navMain.classList.remove("menu--nojs");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("menu--closed")) {
+    navMain.classList.remove("menu--closed");
+    navMain.classList.add("menu--opened");
+  } else {
+    navMain.classList.add("menu--closed");
+    navMain.classList.remove("menu--opened");
+  }
+});
